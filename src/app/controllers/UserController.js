9661af5b71f3f09337d610a85951d9d181042da2
Fabio Focking <focking@edu.univali.br>
2020-05-22 19:53:42 -0300

    Criado o controller User

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,20 @@ import User from "../models/User";
 
 class UserController {
     async store(req, res) {
-        const  user = await User.create(req.body);
 
-        return res.json(user);
+        const userExists = await User.findOne({ where: { emailUser: req.body.emailUser} });
+
+        if(userExists) {
+            return res.status(400).json({ erro: 'Email já existe, favor cadastrar outro'});
+        }
+
+        const {id, nomeUser, emailUser} = await User.create(req.body);
+
+        return res.json({
+            id,
+            nomeUser,
+            emailUser,
+        });
     }
 }
 export default new UserController();
