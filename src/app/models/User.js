91a78efb5acb8c32834951061684d10a34313dfc
Fabio Focking <focking@edu.univali.br>
2020-05-24 15:16:03 -0300

    Adicionada a dependencia para gerar a hash de passwords: bcrypt. Alerado o Model User.js para gerar a senha criptografada.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,15 +1,23 @@
 import Sequelize, { Model } from "sequelize";
+import bcrypt from 'bcryptjs';
 class User extends Model {
     static init(sequelize) {
         super.init({
             nomeUser: Sequelize.STRING,
             emailUser: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             passwordUser: Sequelize.STRING,
             tipoUser: Sequelize.BOOLEAN,
         }, {
             sequelize,
             }
         );
+        this.addHook('beforeSave',async (user) => {
+            if (user.password) {
+                user.passwordUser = await bcrypt.hash(user.password, 8);
+            }
+        })
+        return this;
     }
 }
 
