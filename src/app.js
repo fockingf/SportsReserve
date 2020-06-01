45b84e32a9a8555df7d4fd68a6f46404f3a46b0b
Fabio Focking <focking@edu.univali.br>
2020-06-01 14:33:08 -0300

    adicionado pacotes para tratamento de erros na aplicação.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry'
 import './database';
 
 class App {
     constructor() {
         this.server = express();
+        Sentry.init(sentryConfig);
         this.middlewares();
         this.routes();
+        this.exeptionHandler();
+
     }
 
     middlewares(){
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 
@@ -18,6 +26,16 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exeptionHandler() {
+        this.server.use(async (error, request, response, next) => {
+            const errors = await new Youch(error, request).toJSON();
+
+            return response.status(500).json(errors);
+
+        })
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://57b6e721fbc14a448b9366846f3fd4bd@o401241.ingest.sentry.io/5260585',
+}
